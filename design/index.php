516835336086ebb16396344686e0a0ba5adf835b
Harkil08 <92804807+Harkil08@users.noreply.github.com>
2022-05-25 20:18:32 +0200

    Add files via upload

---
+++ design/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="cs-cz">
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="UNOB-Kniha žádostí" />
+    <meta name="keywords" content="UNOB, kniha, žádostí" />
+    <title>Kniha žádostí</title>
+    
+    <link rel="shortcut icon" href="/design/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="css/zaklad.css" />
+    
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- <script src="/../../www/js/zaklad.js"></script>-->
+
+  </head>
+  <body>
+	<div class="bg"></div>
+	<div class="menu">
+		<ul>
+			<img src="img/logo3.png">
+			<li class="active"><a style="color:black;">Podání žádosti</a></li>
+			<li><a>Moje žádosti</a></li>
+			</ul>
+			<div class="right">
+			<a><i class="icon-cog2"></i></a>
+			<a><i class="icon-log-out"></i></a>
+			</div>
+	</div>
+	<div class="menu-info">
+		<div class="menu-info-text">Uživatel: čet. David Nétek
+		<div class="menu-info-text2">Odhlášení za: 47:52</div></div>
+	</div>
+
+<div class="sekce">
+<div class="sekce-header">
+Podání žádosti
+</div>
+<div class="button" style="margin-top:35px;">Řádná dovolená</div>
+<div class="button">Služební volno</div>
+<div class="button">Náhradní volno</div>
+<div class="button">Zahraniční služební cesta</div>
+<div class="button">Jiná žádost</div>
+<div class="button">Práce přesčas</div>
+<div class="button">Zvláštní dovolená</div>
+<div class="button">Studijní volno</div>
+</div>
+
+
+
+<div class="footer">
+© 2021 Univerzita obrany | Kounicova 65, 662 10 Brno, Czech Republic
+</div>
+
+</body>
+</html>
